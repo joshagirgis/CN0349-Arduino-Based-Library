92be44842dfb5f8be594e1131b2d418c8b581d58
joshagirgis <35814116+joshagirgis@users.noreply.github.com>
2021-08-24 07:15:28 -0400

    Update CN0349.cpp

--- CN0349/CN0349.cpp
+++ CN0349/CN0349.cpp
@@ -1,7 +1,7 @@
 /*
   CN0349.cpp
   MIT License
-Copyright (c) 2017 FESLab
+Copyright (c) 2017 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
 and associated documentation files (the "Software"), to deal in the Software without restriction, 
 including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
